de4cf01b3864f3c0f3dcaaf5cbc3e903ba85b357
Sergio <sergiogarciacotos@gmail.com>
2020-09-08 23:21:10 +0200

    fixed algebraictoLERF

--- mainprogram.java
+++ mainprogram.java
@@ -1,6 +1,5 @@
 public class mainprogram{
     public static void main(String[] args) {
         MainWindow mainwindow = new MainWindow();
-        System.out.println("caca " + Translator.algebraicToLERF("a1"));
     }
 }

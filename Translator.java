0b31a08a5b569352ab00417de4e54d5e5ee556f8
Sergio <sergiogarciacotos@gmail.com>
2020-09-08 22:32:27 +0200

    changes on translation of algebraic moves

---
+++ Translator.java
@@ -0,0 +1,6 @@
+public class Translator {
+	public static int algebraicToLERF(String algebraic){
+		// a = 0, h = 7, each rank += 8
+		return (Character.toLowerCase((algebraic.charAt(0))) - 97) * 8 * (Integer.parseInt(algebraic.substring(1,1)) - 1);
+	}
+}
--- mainprogram.java
+++ mainprogram.java
@@ -1,5 +1,6 @@
 public class mainprogram{
     public static void main(String[] args) {
         MainWindow mainwindow = new MainWindow();
+        System.out.println("caca " + Translator.algebraicToLERF("a1"));
     }
 }

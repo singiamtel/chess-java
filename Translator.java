08676c58a0db867200493bc571645ea23833f6f3
Sergio <sergiogarciacotos@gmail.com>
2020-09-10 01:42:11 +0200

    started logic on Translator

--- Translator.java
+++ Translator.java
@@ -1,10 +1,10 @@
 public class Translator {
-	public static int algebraicToLERF(String algebraic){
+	public static int coordinateToLERF(String algebraic){
 		// a = 0, h = 7, each rank += 8
 		// TODO: clear up
 		return (Character.toLowerCase((algebraic.charAt(0))) - 97) + 8 * (Integer.parseInt(algebraic.substring(1,2)) - 1);
 	}
-	public static String LERFtoAlgebraic(int LERF){
+	public static String LERFtoCoordinate(int LERF){
 		int number = LERF / 8 + 1;
 		int letterCode = LERF % 8;
 		char letter;
@@ -41,5 +41,46 @@ public class Translator {
 		}
 		return (Character.toString(letter) + Integer.toString(number));
 	}
-	// public static 
+	public static String LERFtoAlgebraic(Move move, Game game){
+		String ret;
+		if(game.getPieceAtSquare(move.getFrom()) == Pieces.BLACK_KING 
+		|| game.getPieceAtSquare(move.getFrom()) == Pieces.WHITE_KING){
+			//enroque
+			if((move.getFrom()-move.getTo()) == 2){
+				ret = "O-O";
+			}
+			else{
+				ret = "O-O-O";
+			}
+
+		}
+
+
+		if(PieceLogic.isMate(game, move)){
+			ret += "#";
+		}
+		else if(PieceLogic.isCheck(game, move)){
+			ret += "+";
+		}
+
+		return "return";
+	}
 }
+
+/*
+
+20 26
+e3 c4 
+
+e3xc4
+
+Enroque O-O
+Come x
+Jaque +
+Mate #
+Promocion =Q
+Distinción de que pieza come kexc5
+ka8xe6+
+exa8=Q+
+
+*/

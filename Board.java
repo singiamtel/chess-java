e88886bb74cc7e5fa3a9864eca5ba92a276d1405
Sergio <sergiogarciacotos@gmail.com>
2020-09-09 19:28:20 +0200

    finished refactor

---
+++ Board.java
@@ -0,0 +1,21 @@
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+
+public class Board extends JPanel{
+
+    private static final long serialVersionUID = 5071359096090410155L;
+
+    private JPanel[][] squares = new JPanel[8][8];
+    private void initUI(){
+    }
+    Board(){
+        this.setLayout(new GridLayout(8,8));
+        for(int i=0; i<8; ++i){
+            for(int j=0; j<8; ++j){
+                // White squares are even, black squares are odd
+                squares[i][j] = new Square((i+j) % 2 ==0 ? true:false,Pieces.WHITE_KING,true,true,true);
+                this.add(squares[i][j]);
+            }
+        }
+    }
+}
---
+++ MainProgram.java
@@ -0,0 +1,5 @@
+public class MainProgram{
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
+}

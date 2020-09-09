41c42f3a005ee6cb85763e7d587ec5eee12fd618
Sergio <sergiogarciacotos@gmail.com>
2020-09-10 01:45:57 +0200

    created dummy methods

--- PieceLogic.java
+++ PieceLogic.java
@@ -1,2 +1,16 @@
 public class PieceLogic {
+	public static Boolean isValid(Game game, Move move){
+		return false;
+	}
+	public static Boolean isCheck(Game game, Move move){
+		return false;
+	}
+	public static Boolean isMate(Game game, Move move){
+		if(!isCheck(game, move)) return false;
+
+		return false;
+	}
+	public static Boolean isStaleMate(Game game, Move move){
+		return false;
+	}
 }

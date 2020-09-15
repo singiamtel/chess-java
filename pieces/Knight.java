acb233cc72945a35b6dccaa3cbb51dfcf263fe90
Sergio <sergiogarciacotos@gmail.com>
2020-09-15 23:20:19 +0200

    created movegen

--- pieces/Knight.java
+++ pieces/Knight.java
@@ -37,19 +37,22 @@ public class Knight extends Piece {
 	public ArrayList<Move> generateMoves(Game game){
 		ArrayList<Move> generatedMoves = new ArrayList<Move>();
 		for (Pair<Integer, Integer> move : moves) {
-			
+			Pair<Integer, Integer> attempt = this.position.substractPair(move);
+			if(! this.validateMove(game, new Move(attempt))){
+				continue;
+			} else {
+				if (this.isMoveEating(game, new Move(attempt))) {
+					generatedMoves.add(new Move(attempt, null, false, true));
+				} else
+					generatedMoves.add(new Move(attempt, null, false, false));
+			}
+
 		}
 		return generatedMoves;
 	}
 }
 /*
--17 -15
--10 -6
-+6 +10
-+15 +17
-
-(2,1)(2,-1)
-(1,2)(-1,2)
-(-2,1)(-2,-1)
-(1,-2)(-1,-2)
-*/
+ * -17 -15 -10 -6 +6 +10 +15 +17
+ * 
+ * (2,1)(2,-1) (1,2)(-1,2) (-2,1)(-2,-1) (1,-2)(-1,-2)
+ */

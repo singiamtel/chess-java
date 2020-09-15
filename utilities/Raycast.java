26903de6f1bf759d31ce05c866b9b31256ab1c54
Sergio <sergiogarciacotos@gmail.com>
2020-09-15 23:21:24 +0200

    moved validation responsabilities to validate and fixed off-by-one bug

--- utilities/Raycast.java
+++ utilities/Raycast.java
@@ -7,51 +7,56 @@ import controller.Move;
 import pieces.Piece;
 
 public class Raycast {
-	public static ArrayList<Move> raycast(Game game, Piece piece, Direction dir){
+	public static ArrayList<Move> raycast(Game game, Piece piece, Direction dir) {
 		ArrayList<Move> moveArray = new ArrayList<Move>();
 		Pair<Integer, Integer> position = piece.getPosition();
 		int x = position.getFirst();
 		int y = position.getSecond();
 
-		switch(dir){
+		switch (dir) {
 			case EAST:
-				recursiveRaycast(moveArray, game, piece, x, y, 0, 1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, 0, 1, x, y);
 				break;
 			case NORTH:
-				recursiveRaycast(moveArray, game, piece, x, y, 1, 0, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, 1, 0, x, y);
 				break;
 			case NORTHEAST:
-				recursiveRaycast(moveArray, game, piece, x, y, 1, 1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, 1, 1, x, y);
 				break;
 			case NORTHWEST:
-				recursiveRaycast(moveArray, game, piece, x, y, 1, -1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, 1, -1, x, y);
 				break;
 			case SOUTH:
-				recursiveRaycast(moveArray, game, piece, x, y, -1, 0, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, -1, 0, x, y);
 				break;
 			case SOUTHEAST:
-				recursiveRaycast(moveArray, game, piece, x, y, -1, 1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, -1, 1, x, y);
 				break;
 			case SOUTHWEST:
-				recursiveRaycast(moveArray, game, piece, x, y, -1, -1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, -1, -1, x, y);
 				break;
 			case WEST:
-				recursiveRaycast(moveArray, game, piece, x, y, 0, -1, x, y);			
+				recursiveRaycast(moveArray, game, piece, x, y, 0, -1, x, y);
 				break;
 		}
 		return moveArray;
 	}
-	private static void recursiveRaycast(ArrayList<Move> moveArray, Game game, Piece piece, int x, int y, int addX, int addY, int originalX, int originalY){
-		x+=addX;
-		y+=addY;
-		if(x<=0 || y<=0) return;
-		if(x>=8 || y>=8) return;
-		if(game.getPieceAtSquare(8*x + y) == null){
-			moveArray.add(new Move(8*originalX + originalY , 8*x + y, null, false, false));
-			recursiveRaycast(moveArray, game, piece, x, y, addX, addY ,originalX, originalY);
-		}
-		else if(game.getPieceAtSquare(8*x + y).isWhite() != piece.isWhite()){
-			moveArray.add(new Move(8*originalX + originalY , 8*x + y, null, false, true));
+
+	private static void recursiveRaycast(ArrayList<Move> moveArray, Game game, Piece piece, int x, int y, int addX,
+			int addY, int originalX, int originalY) {
+		x += addX;
+		y += addY;
+		if (x < 0 || y < 0)
+			return;
+		if (x >= 8 || y >= 8)
+			return;
+		if (piece.validateMove(game, new Move(x, y))) {
+			if (piece.isMoveEating(game, new Move(x, y))) {
+				moveArray.add( new Move(new Pair<Integer, Integer>(8 * originalX + originalY, 8 * x + y), null, false, true));
+			} else {
+				moveArray.add( new Move(new Pair<Integer, Integer>(8 * originalX + originalY, 8 * x + y), null, false, false));
+				recursiveRaycast(moveArray, game, piece, x, y, addX, addY, originalX, originalY);
+			}
 		}
 	}
 }

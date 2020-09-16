b5cbdf62dddbe79606f20d88744ebe82ebef594d
Marcos <marcca05@ucm.es>
2020-09-16 21:57:16 +0000

    added Controller to mainprogram

--- MainProgram.java
+++ MainProgram.java
@@ -1,6 +1,8 @@
+import controller.Controller;
 import gui.MainWindow;
-public class MainProgram{
+public class MainProgram {
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
+        Controller controller = new Controller();
     }
 }

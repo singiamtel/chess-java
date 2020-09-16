c4ca4f5b8d292948cb43bbad7ee67951601a31e1
Sergio <sergiogarciacotos@gmail.com>
2020-09-16 22:09:13 +0200

    indented file

--- pieces/Knight.java
+++ pieces/Knight.java
@@ -12,19 +12,13 @@ import utilities.Pair;
 public class Knight extends Piece {
 
 	public static final ArrayList<Pair<Integer, Integer>> moves = new ArrayList<Pair<Integer, Integer>>(List.of(
-		new Pair<Integer, Integer>(2,1),
-		new Pair<Integer, Integer>(2,-1),
-		new Pair<Integer, Integer>(1,2),
-		new Pair<Integer, Integer>(1,-2),
-		new Pair<Integer, Integer>(-2,1),
-		new Pair<Integer, Integer>(-2,-1),
-		new Pair<Integer, Integer>(-1,-2),
-		new Pair<Integer, Integer>(-1,-2)
-		));
+			new Pair<Integer, Integer>(2, 1), new Pair<Integer, Integer>(2, -1), new Pair<Integer, Integer>(1, 2),
+			new Pair<Integer, Integer>(1, -2), new Pair<Integer, Integer>(-2, 1), new Pair<Integer, Integer>(-2, -1),
+			new Pair<Integer, Integer>(-1, -2), new Pair<Integer, Integer>(-1, -2)));
 
 	public Knight(Boolean isWhite, Pair<Integer, Integer> position) {
 		this.isWhite = isWhite;
-		this.position= position;
+		this.position = position;
 	}
 
 	public ImageIcon getImage() {
@@ -34,21 +28,24 @@ public class Knight extends Piece {
 			return new ImageIcon("img/black_knight.png");
 		}
 	}
-	public ArrayList<Move> generateMoves(Game game){
+
+	public ArrayList<Move> generateMoves(Game game) {
 		ArrayList<Move> generatedMoves = new ArrayList<Move>();
 		for (Pair<Integer, Integer> move : moves) {
 			Pair<Integer, Integer> attempt = this.position.substractPair(move);
-			if(attempt.getFirst() < 0 || attempt.getFirst() >=8) continue;
-			if(attempt.getSecond() < 0 || attempt.getSecond() >=8) continue;
-			if(! this.validateMove(game, new Move(attempt))){
+			if (attempt.getFirst() < 0 || attempt.getFirst() >= 8)
+				continue;
+			if (attempt.getSecond() < 0 || attempt.getSecond() >= 8)
+				continue;
+			if (!this.validateMove(game, new Move(attempt))) {
 				continue;
 			} else {
 				if (this.isMoveEating(game, new Move(attempt))) {
-				System.out.println("adding: eating " + attempt.getFirst() + " " + attempt.getSecond());
+					System.out.println("adding: eating " + attempt.getFirst() + " " + attempt.getSecond());
 					generatedMoves.add(new Move(attempt, null, false, true));
 				} else
-				System.out.println("adding: not eating " + attempt.getFirst() + " " + attempt.getSecond());
-					generatedMoves.add(new Move(attempt, null, false, false));
+					System.out.println("adding: not eating " + attempt.getFirst() + " " + attempt.getSecond());
+				generatedMoves.add(new Move(attempt, null, false, false));
 			}
 
 		}

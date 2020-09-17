79bffd11fa2e87f5f6da0c41d67fe31676935d05
Marcos <marcca05@ucm.es>
2020-09-17 02:56:18 +0000

    added dummy line

--- MainProgram.java
+++ MainProgram.java
@@ -8,5 +8,11 @@ import utilities.Pair;
 public class MainProgram {
     public static void main(String[] args) {
         Controller controller = Controller.getController();
+        Game game = new Game(true);
+        ArrayList<Move> awd = game.generateAllColourMoves(true);
+        for(int i=0;i<awd.size();++i){
+            System.out.println("move " + awd.get(i) + ": " + awd.get(i).getFrom() + " " + awd.get(i).getTo());
+        }
+        System.out.println("dummy");
     }
 }

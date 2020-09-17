83cb5b7d5f39a381faf221c1a8fe2ff70f53d01a
Sergio <sergiogarciacotos@gmail.com>
2020-09-17 02:41:04 +0200

    added test

--- MainProgram.java
+++ MainProgram.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 import controller.Controller;
+import controller.Game;
+import controller.Move;
 import gui.MainWindow;
+import utilities.Pair;
 public class MainProgram {
     public static void main(String[] args) {
         Controller controller = new Controller();
         MainWindow mainWindow = new MainWindow(controller);
+        Game game = new Game(true);
+        ArrayList<Move> awd = game.generateAllColourMoves(true);
+        for(int i=0;i<awd.size();++i){
+            System.out.println("move " + awd.get(i) + ": " + awd.get(i).getFrom() + " " + awd.get(i).getTo());
+        }
     }
 }

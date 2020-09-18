9e24d5f134cc21ab04137dc577453946f5043634
Sergio <sergiogarciacotos@gmail.com>
2020-09-18 02:14:14 +0200

    clean up

--- utilities/Translator.java
+++ utilities/Translator.java
@@ -7,8 +7,6 @@ import pieces.King;
 
 public class Translator {
 	public static int coordinateToLERF(String algebraic){
-		// a = 0, h = 7, each rank += 8
-		// TODO: clear up
 		return (Character.toLowerCase((algebraic.charAt(0))) - 97) + 8 * (Integer.parseInt(algebraic.substring(1,2)) - 1);
 	}
 	public static String LERFtoCoordinate(int LERF){

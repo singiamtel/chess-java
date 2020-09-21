bec09429c1a6da33c4a982dbf002c61fd0d248e1
Sergio <sergiogarciacotos@gmail.com>
2020-09-21 11:29:27 +0200

    updated function

--- gui/GameController.java
+++ gui/GameController.java
@@ -8,22 +8,37 @@ public class GameController {
 	private Game game;
 	private Board board;
 	private boolean isPieceSelected;
+	private Pair<Integer, Integer> pieceSelected;
 
 	public void checkIfSync(){
 		//TODO: exception
 	}
 	public boolean makeMove(Pair<Integer, Integer> from, Pair<Integer, Integer> to){
-		if(game.validateMove(new Move(from, to))){
-			game.makeMove(new Move(from, to));
-			board.makeMove(new Move(from, to));
+		Move move = new Move(from, to);
+		if(game.validateMove(move)){
+			game.makeMove(move);
+			board.makeMove(move);
 			// change turn
 			return true;
 		}
 		return false;
 	}
 
-	public void update(){
+	private void update(){
 		board.clearMoveMarks();
 		game.update();
+	}
+	private void generateMoveMarks(){
+		
+	}
+
+	public void handleClick(Pair<Integer, Integer> on){
+		if(!isPieceSelected && board.getSquareAt(on) != null){
+			this.isPieceSelected = true;
+			this.pieceSelected = on;
+			generateMoveMarks();
+
+			// Make green marks
+		}
 	}
 }
